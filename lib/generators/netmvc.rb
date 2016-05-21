2e3dfd133a5de7389c2b63c84fd5c4ae573cfc8b
Nic Jackson <jackson.nic@gmail.com>
2016-05-21 19:55:11 +0100

    updates

--- lib/generators/netmvc.rb
+++ lib/generators/netmvc.rb
@@ -15,14 +15,14 @@ module Minke
 
       config.build_settings.build_commands = Minke::Generators::BuildCommands.new.tap do |bc|
         bc.fetch = [['/bin/bash', '-c', 'dotnet restore --packages ./.nuget']]
-        bc.build = [['/bin/bash', '-c', 'NUGET_PACKAGES=./.nuget dotnet build']]
         bc.test = [['/bin/bash', '-c', 'NUGET_PACKAGES=./.nuget dotnet test']]
+        bc.build = [['/bin/bash', '-c', 'NUGET_PACKAGES=./.nuget dotnet build -c Release']]
       end
 
       config.build_settings.docker_settings = Minke::Generators::DockerSettings.new.tap do |bs|
         bs.image = 'microsoft/dotnet:latest'
-        bs.binds = ["<%= src_root %>:/src"]
-        bs.working_directory = '/src' # working directory is added via a class patch to enable lazy load
+        bs.binds = ["<%= src_root %>:/<%= application_name %>"]
+        bs.working_directory = '/<%= application_name %>' # working directory is added via a class patch to enable lazy load
       end
 
       Minke::Generators.register config
--- lib/generators/netmvc/version.rb
+++ lib/generators/netmvc/version.rb
@@ -1,7 +1,7 @@
 module Minke
   module Generators
     module NetMVC
-      VERSION = "0.2.0"
+      VERSION = "0.3.0"
     end
   end
 end
