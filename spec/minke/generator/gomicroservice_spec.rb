69730a00bf003fcee05b05372de33f2a63f2d3ec
Nic Jackson <jackson.nic@gmail.com>
2016-05-21 17:28:13 +0100

    updated specs

--- spec/minke/generator/gomicroservice_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe Minke::Generators::GoMicroservice do
-  it 'has a version number' do
-    expect(Minke::Generators::GoMicroservice::VERSION).not_to be nil
-  end
-end
---
+++ spec/minke/generator/netmvc_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe Minke::Generators::NetMVC do
+  it 'has a version number' do
+    expect(Minke::Generators::NetMVC::VERSION).not_to be nil
+  end
+end
